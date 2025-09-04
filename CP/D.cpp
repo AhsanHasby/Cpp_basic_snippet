c0bcc8f5017269f52613013e637992dac4f58f2e
Ahsan Hasby <mahahsanhasby@gmail.com>
2025-09-04 21:18:08 +0600

    Basic snippet of mine

---
+++ CP/D.cpp
@@ -0,0 +1,36 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+using ll = long long;
+using str = string;
+
+//Fast_IO:
+#define WAKE             ios_base::sync_with_stdio(false); 
+#define UP_TO            cin.tie(nullptr);
+#define REALITY          cout.tie(nullptr);
+
+//define:
+#define endL             "\n"
+#define yes              cout << "YES\n"
+#define no               cout << "NO\n"
+
+
+//Here is the code:
+void naruto() {
+    
+    
+}
+// Bismillah...
+int main() {
+    WAKE UP_TO REALITY
+    
+    //test cases
+    int test = 1;
+    cin >> test;
+    for(int i = 1; i <= test; i++) {
+        // cout << "Case " << i <<": ";
+        naruto();
+    }
+    return 0;
+}
