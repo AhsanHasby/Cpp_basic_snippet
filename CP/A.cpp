cfd8962fa4f3bfd004b2c67716aa7a4a0477d887
Ahsan Hasby <mahahsanhasby@gmail.com>
2025-10-27 08:51:25 +0600

    Template modified

--- CP/A.cpp
+++ CP/A.cpp
@@ -18,6 +18,7 @@ using vll  = vector<ll>;
 const char endL = '\n';
 const int  inf  = 0x3f3f3f3f;
 const int  MAX  = 200000; //if given
+const long long MOD  = (1e9+7); //if given
 // =================== Fast IO ===================
 #define WAKE    ios_base::sync_with_stdio(false);
 #define UP_TO   cin.tie(nullptr);
@@ -26,7 +27,7 @@ const int  MAX  = 200000; //if given
 #define all(x)  (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
 #define sz(x)   (int)(x.size())
-#define pb(x)    push_back(x)
+#define eb       emplace_back
 
 #define print(arr) for(auto &it:(arr)) cout << it << " "; cout << endL;
 #define yes     cout << "YES\n"
@@ -34,7 +35,35 @@ const int  MAX  = 200000; //if given
 //================= MAIN CODE ===============
 
 void DomainExpansion() {
+    str s; cin >> s;
+    ll n = sz(s), max_1 = 0;
 
+    for(int i = 0; i < n; i++) {
+        if(s[i] == '1') 
+            max_1++;
+        else 
+            break;
+    }
+    for(int i = n-1; i > 0 && max_1 != n; i--) {
+        if(s[i] == '1') 
+            max_1++;
+        else 
+            break;
+    }
+    ll cnt = 0;
+    for(int i = 0; i < n; i++) {
+        if(s[i] == '1') 
+            cnt++;
+        else 
+            cnt = 0;
+        max_1 = max(max_1, cnt);
+    }
+    ll area = 0;
+    if(max_1 == n) area = max_1*max_1;
+    else if(max_1 == 1) area = 1;
+    else if(max_1 != 0) area = ((max_1+1)/2)*((max_1+2)/2);
+
+    cout << area << endL; 
 
 }
 // Bismillah...
